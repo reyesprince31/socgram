2574f17d8f043b87ae69caca727d4d33b3c7ab01
Prince Reyes <krayzerprince@gmail.com>
2023-12-19 12:47:57 +0800

    init build prod ready

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,9 +1,4 @@
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  useInfiniteQuery,
-} from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import {
   createNewAccount,
   createPost,
